4117a63951273ddbad20fda94dcc0fd3d151872a
renato-scaroni <scaroni@scaroni-N56VJ.(none)>
2013-10-02 15:33:26 -0300

    commitando pro dudu, BFS funciona, mas quebra na hora de achar caminhos em alguns casos. Investigar o pq

--- Agente.py
+++ Agente.py
@@ -5,7 +5,7 @@ class Agente(object):
 
 	def GetDirFromDiff(self, d):
 		delta = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-		direc = ['B', 'D', 'E', 'C']
+		direc = ['D', 'B', 'C', 'E']
 		for i, j in zip(delta, direc):
 			if(i[0] == d[0] and i[1] == d[1]):
 				return j
@@ -15,7 +15,7 @@ class Agente(object):
 		x = c[0] - b[0]
 		y = c[1] - b[1]
 		delta = [[0, 1], [1, 0], [-1, 0], [0, -1]]
-		direc = ['B', 'D', 'E', 'C']
+		direc = ['D', 'B', 'C', 'E']
 		for i, j in zip(delta, direc):
 			if(i[0] == x and i[1] == y):
 				return j
@@ -24,11 +24,16 @@ class Agente(object):
 	def BuildPath(self, parnt, s, f):
 		v = f
 		path = []
-		print parnt[f], f
+		custo = 0
+		# path = [v]
+		# print f
 		while not v == parnt[v]:
-			path.append(self.GetDirFromVecs(v, parnt[v]))
+			path.append(self.GetDirFromVecs(parnt[v], v))
+			# path.append(parnt[v])
 			v = parnt[v]
-		return path
+			custo -= 1
+		path.reverse()
+		return custo, path
 
 	#Funcao que realiza a busca em largura 
 	def BFS (self, mina, n, g):
@@ -43,18 +48,24 @@ class Agente(object):
 		s = 0, 0
 		parnt = {}
 		parnt[(0,0)] = (0,0)
+		custo = 0
 		while (len(fila) > 0):
-			curr = fila[0]
-			del(fila[0])
+			curr = fila.pop(0)
 			if(curr == (0,0)) and (len(o) == g):
+				ret = self.BuildPath(parnt, s, curr)
+				path += ret [1]
+				custo += ret[0]
 				if self._debug :
 					print "estado final, saindo "+str(curr) + str(o)
 				break
 			if(mina.mapa[curr[0]][curr[1]] == '*' and not curr in o and len(o) < g):
-				path += self.BuildPath(parnt, s, curr)
+				ret = self.BuildPath(parnt, s, curr)
+				path += ret [1]
+				custo += ret[0]
 				s = curr
 				parnt[s] = s
 				path += 'P'
+				custo += 4*n
 				o.append(curr)
 			estadosVisitados.append((curr, []))
 			for a in o:
@@ -69,8 +80,9 @@ class Agente(object):
 					if (next[0] >= 0 and next[0] < n) and (next[1] >= 0 and next[1] < n):
 						if not mina.mapa[next[0]][next[1]] == '1':
 							parnt[next] = curr
+							# print str(curr) + "->" + str(next)
 							fila.append(next)
-		return path
+		return custo, path
 
 	def __init__(self):
 		self._debug  = False
